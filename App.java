db44ec4006f36e58c2ae420a82479927997fb9da
mahevish <mahevish@gmail.com>
2026-09-19 17:21:34 +0530

    Fix settings.gradle content

---
+++ App.java
@@ -0,0 +1,9 @@
+cat << 'EOF' > src/main/java/com/example/App.java
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+EOF
